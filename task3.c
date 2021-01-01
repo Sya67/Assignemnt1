9110ef8104271fca52515b6df1582b489a0698b7
Sya67 <76838305+Sya67@users.noreply.github.com>
2021-01-02 02:11:47 +1100

    Add files via upload

---
+++ task3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+const int N = 6;
+float data [6] = {1.2, 8.4, 10.8, -0.4, -8.234, 3.1};
+int i;
+float max = data[0]
+for (i = 0; i<N; i++){
+    if (data[i] > max)
+        max = data[i];
+        maxindex = i;
+
+    return max;
+}
+printf("Largest in given array is %f found at index %d\n", max, i0);
+
+return 0;
+}
